f751f863100124799a1792b3f1df00d78e4bfe7c
lmalunin <lmalunin@gmail.com>
2022-09-17 09:31:42 +0300

    index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CalendarModule} from "primeng/calendar";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
